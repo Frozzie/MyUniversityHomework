2a911f333b7b235c97cf274b87741cfc229d12a5
Frozzie <yasha.petrukhin@gmail.com>
2024-02-17 16:30:24 +0300

    append access functions

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 void test_pushBack_emptyVector()
 {
     vector v = createVector(0);
+
     pushBack(&v, 1);
     assert(v.size == 1);
 }
@@ -13,6 +14,7 @@ void test_pushBack_emptyVector()
 void test_pushBack_fullVector()
 {
     vector v = createVector(1);
+
     pushBack(&v, 1);
     pushBack(&v, 1);
     assert(v.size == 2);
@@ -30,11 +32,59 @@ void test_popBack_notEmptyVector()
     assert(v.capacity == 1);
 }
 
+void test_atVector_notEmptyVector()
+{
+    vector v = createVector(2);
+    
+    pushBack(&v, 1);
+    int *answ = atVector(&v, 0);
+
+    assert(*answ == 1);
+}
+
+void test_atVector_requestToLastElement()
+{
+    vector v = createVector(2);
+    
+    pushBack(&v, 1);
+    pushBack(&v, 2);
+    int *answ = atVector(&v, 1);
+
+    assert(*answ == 2);
+}
+
+void test_back_oneElementInVector()
+{
+    vector v = createVector(2);
+    
+    pushBack(&v, 1);
+    pushBack(&v, 2);
+
+    int *answ = back(&v);
+
+    assert(*answ == 2);
+}   
+
+void test_front_oneElementInVector()
+{
+    vector v = createVector(2);
+    
+    pushBack(&v, 1);
+
+    int *answ = back(&v);
+
+    assert(*answ == 1);
+}
+
 void test() 
 {
     test_pushBack_emptyVector();
     test_pushBack_fullVector();
     test_popBack_notEmptyVector();
+    test_atVector_notEmptyVector();
+    test_atVector_requestToLastElement();
+    test_back_oneElementInVector();
+    test_front_oneElementInVector();
     // последующие тесты
 }
 
